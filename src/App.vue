f6d388ec350b73680b8c9a43c313f8b0bd7a610b
Thomas Lutaster <lutaster.t@gmail.com>
2022-08-31 16:26:31 +0200

    Add quantity in cart

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,45 @@
 <script setup lang="ts">
-  import TheHeader from './components/Header.vue'
-  import TheFooter from './components/Footer.vue'
-  import Shop from './components/Shop/Shop.vue'
-  import Cart from './components/Cart/Cart.vue'
-  import data from './data/product'
-  import { reactive } from 'vue'
-  import type { ProductInterface } from './interfaces/product.interface'
+  import TheHeader from './components/Header.vue';
+  import TheFooter from './components/Footer.vue';
+  import Shop from './components/Shop/Shop.vue';
+  import Cart from './components/Cart/Cart.vue';
+  import data from './data/product';
+  import { reactive } from 'vue';
+  import type { ProductCartInterface, ProductInterface } from './interfaces';
 
   const state = reactive<{
     products: ProductInterface[]
-    cart: ProductInterface[]
+    cart: ProductCartInterface[]
   }>({
     products: data,
     cart: []
-  })
+  });
 
   function addProductToCart(productId: number): void {
   const product = state.products.find((product) => product.id === productId);
-  if (product && !state.cart.find((product) => product.id === productId)) {
-    state.cart.push({ ...product });
+  if (product) {
+    const productInCart = state.cart.find(
+      (product) => product.id === productId
+    );
+    if (productInCart) {
+      productInCart.quantity++;
+    } else {
+      state.cart.push({ ...product, quantity: 1 });
+    }
   }
 }
 
 function removeProductFromCart(productId: number): void {
-  state.cart = state.cart.filter((product) => product.id !== productId);
+  const productFromCart = state.cart.find(
+    (product) => product.id === productId
+  );
+  if (productFromCart) {
+    if (productFromCart?.quantity === 1) {
+    state.cart = state.cart.filter((product) => product.id !== productId);
+  } else {
+    productFromCart.quantity--;
+  }
+  }
 }
 </script>
 
